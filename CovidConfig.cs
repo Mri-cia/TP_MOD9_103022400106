1509b0404ab0d97b5018993e589a2313fd96b8d4
Mri-cia <rawikara8c@gmail.com>
2026-04-26 15:55:53 +0700

    Menambahkan Implementasi Runtime Configuration

---
+++ CovidConfig.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+public class CovidConfig
+{
+    [JsonIgnore]
+    private string _filePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "covid_config.json");
+
+    [JsonPropertyName("satuan_suhu")]
+    public string SatuanSuhu { get; set; }
+
+    [JsonPropertyName("batas_hari_deman")]
+    public int BatasHariDeman { get; set; }
+
+    [JsonPropertyName("pesan_ditolak")]
+    public string PesanDitolak { get; set; }
+
+    [JsonPropertyName("pesan_diterima")]
+    public string PesanDiterima { get; set; }
+
+    public CovidConfig() 
+    {
+        // Set default values
+        SatuanSuhu = "celsius";
+        BatasHariDeman = 14;
+        PesanDitolak = "Anda tidak diperbolehkan masuk ke dalam gedung ini";
+        PesanDiterima = "Anda dipersilahkan untuk masuk ke dalam gedung ini";
+
+    }
+
+    public void UbahSatuan()
+    {
+        if (SatuanSuhu.ToLower() == "celsius")
+        {
+            SatuanSuhu = "fahrenheit";
+        }
+        else
+        {
+            SatuanSuhu = "celsius";
+        }
+    }
+
+    public void LoadConfig()
+    {
+        if (File.Exists(_filePath))
+        {
+            try
+            {
+                string jsonString = File.ReadAllText(_filePath);
+                var configFromFile = JsonSerializer.Deserialize<CovidConfig>(jsonString);
+                if (configFromFile != null)
+                {
+                    SatuanSuhu = configFromFile.SatuanSuhu;
+                    BatasHariDeman = configFromFile.BatasHariDeman;
+                    PesanDitolak = configFromFile.PesanDitolak;
+                    PesanDiterima = configFromFile.PesanDiterima;
+                    return;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Gagal membaca konfigurasi dari file: {ex.Message}");
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,1 +1,41 @@
-﻿
+﻿var config = new CovidConfig();
+
+config.LoadConfig();
+
+
+for (int i = 0; i < 2; i++)
+{
+    Console.WriteLine($"Masukkan suhu tubuh Anda ({config.SatuanSuhu}):");
+    double suhu = Convert.ToDouble(Console.ReadLine());
+    Console.WriteLine($"Berapa hari Anda mengalami demam? (batas: {config.BatasHariDeman} hari)");
+    int hariDeman = Convert.ToInt32(Console.ReadLine());
+
+    if (!conditionals(suhu, hariDeman))
+    {
+        Console.WriteLine(config.PesanDitolak);
+    }
+    else
+    {
+        Console.WriteLine(config.PesanDiterima);
+    }
+
+    config.UbahSatuan();
+
+}
+
+bool conditionals(double temp, int days)
+{
+    if (days > config.BatasHariDeman)
+    {
+        return false;
+    }
+    else if (config.SatuanSuhu.ToLower() == "fahrenheit")
+    {
+        if (temp < 97.7 || temp > 99.5) return false;
+    }
+    else if (config.SatuanSuhu.ToLower() == "celsius")
+    {
+        if (temp < 36.5 || temp > 37.5) return false;
+    }
+    return true;
+}
